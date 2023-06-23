c78cb0810b63f94c69cc3fa3351ade38c07f6e76
Nonsomanuel <immannuell234@gmail.com>
2023-06-23 02:17:54 -0700

    another commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ * @n: The last digit gangan
+ *
+ * Return: Value of the last gigit.
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+
+	_putchar(a + '0');
+
+	return (a);
+
+}
