aede91ae830b3ac7850c27fa39d33e2d0c78a9f4
root <immannuell234@gmailo.com>
2023-12-10 11:30:00 -0800

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b) {
+	if (b == NULL)
+		return 0;
+
+	unsigned int result = 0;
+
+	while (*b != '\0') {
+		if (*b == '0' || *b == '1') {
+			result = (result << 1) + (*b - '0');
+		} else {
+		 // If the character is not 0 or 1, return 0 as an error indicator
+		 return 0;
+		}
+		b++;
+	}
+
+	return result;
+}
+
+int main() {
+	const char *binary_str = "110101";
+	unsigned int result = binary_to_uint(binary_str);
+
+	if (result == 0 && binary_str != NULL) {
+		printf("Error: Invalid binary string\n");
+	} else {
+		printf("Decimal representation: %u\n", result);
+	}
+
+	return 0;
+}
