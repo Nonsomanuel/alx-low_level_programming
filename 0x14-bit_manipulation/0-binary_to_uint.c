5298309d64eaaf28be2e7b642331e897b7fafad8
root <immannuell234@gmailo.com>
2024-01-12 17:02:14 -0800

    another commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,35 +8,31 @@
  *
  * Return: unsigned int.
  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int mk;
+	int len, base_two;
 
-unsigned int binary_to_uint(const char *b) {
-	if (b == NULL)
-		return 0;
+	if (!b)
+		return (0);
 
-	unsigned int result = 0;
+	mk = 0;
 
-	while (*b != '\0') {
-		if (*b == '0' || *b == '1') {
-			result = (result << 1) + (*b - '0');
-		} else {
-			// If the character is not 0 or 1, return 0 as an error indicator
-			return 0;
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
 		}
-		b++;
+
+		if (b[len] & 1)
+		{
+			mk += base_two;
+		}
 	}
 
-	return result;
-}
-
-int main() {
-	const char *binary_str = "110101";
-	unsigned int result = binary_to_uint(binary_str);
-
-	if (result == 0 && binary_str != NULL) {
-		printf("Error: Invalid binary string\n");
-	} else {
-		printf("Decimal representation: %u\n", result);
-	}
-
-	return 0;
+	return (mk);
 }
