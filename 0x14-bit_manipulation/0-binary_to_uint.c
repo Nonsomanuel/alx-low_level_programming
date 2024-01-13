88b9048400be4d0f024a95d86bfadcb91d054185
root <immannuell234@gmailo.com>
2024-01-12 16:35:12 -0800

    initial commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
 
 unsigned int binary_to_uint(const char *b) {
 	if (b == NULL)
@@ -10,24 +19,11 @@ unsigned int binary_to_uint(const char *b) {
 		if (*b == '0' || *b == '1') {
 			result = (result << 1) + (*b - '0');
 		} else {
-		 // If the character is not 0 or 1, return 0 as an error indicator
-		 return 0;
+			// If the character is not 0 or 1, return 0 as an error indicator
+			return 0;
 		}
 		b++;
 	}
 
 	return result;
-}
-
-int main() {
-	const char *binary_str = "110101";
-	unsigned int result = binary_to_uint(binary_str);
-
-	if (result == 0 && binary_str != NULL) {
-		printf("Error: Invalid binary string\n");
-	} else {
-		printf("Decimal representation: %u\n", result);
-	}
-
-	return 0;
 }
